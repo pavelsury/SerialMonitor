574f464c44eb640f62cf33e0613fd1d67c909ccc
Pavel Sury <psury@amberg.cz>
2021-11-21 11:14:58 +0100

    Improve script to update version in readme

--- BuildScripts/UpdateVersion.ps1
+++ BuildScripts/UpdateVersion.ps1
@@ -1,9 +1,16 @@
 Param($SolutionDir, $ProjectDir)
 
+$versionPattern = '(\d+)\.(\d+)\.(\d+)'
+
 $appInfoPath = $SolutionDir + 'SerialMonitor.Business\AppInfo.cs'
-$version = (Select-String -Path $appInfoPath -Pattern '(\d+\.)?(\d+\.)?(\*|\d+)').Matches.Value
+$version = (Select-String -Path $appInfoPath -Pattern $versionPattern).Matches.Value
 
 $manifestPath = $ProjectDir + 'source.extension.vsixmanifest'
 $manifestXml = [xml](Get-Content $manifestPath -Raw)
 $manifestXml.PackageManifest.Metadata.Identity.Version = $version
 $manifestXml.save($manifestPath)
+
+$readmePath = $SolutionDir + 'README.md'
+$readmeContent = Get-Content -path $readmePath -Raw
+$readmeContent = $readmeContent -Replace $versionPattern, $version
+[system.io.file]::WriteAllText($readmePath, $readmeContent)
