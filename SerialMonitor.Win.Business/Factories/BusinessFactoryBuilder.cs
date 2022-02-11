d5f5e7b5374f62b17abed4d986ec2620a396490b
Pavel Sury <psury@amberg.cz>
2022-02-11 18:01:13 +0100

    Fix default params

--- SerialMonitor.Win.Business/Factories/BusinessFactoryBuilder.cs
+++ SerialMonitor.Win.Business/Factories/BusinessFactoryBuilder.cs
@@ -4,7 +4,7 @@ namespace SerialMonitor.Win.Business.Factories
 {
     public static class BusinessFactoryBuilder
     {
-        public static async Task InitializeAsync(string settingsFilename, string selectedPort) => await ModelFactory.InitializeAsync(settingsFilename, selectedPort);
+        public static async Task InitializeAsync(string settingsFilename = null, string selectedPort = null) => await ModelFactory.InitializeAsync(settingsFilename, selectedPort);
 
         public static ModelFactory ModelFactory { get; } = new ModelFactory();
     }
