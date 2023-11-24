16267d8809006d72a40bbed0228959327b8b43c3
Pavel Surý <suryp@email.cz>
2023-11-24 19:56:38 +0100

    Drop support for self-contained application

--- BuildScripts/Publish.ps1
+++ BuildScripts/Publish.ps1
@@ -18,11 +18,7 @@ $vsixSrcFilename = $SolutionDir + 'SerialMonitor.VS2022\bin\Publish\SerialMonito
 $vsixDstFilename = $publishDir + 'SerialMonitor2_v' + $version + '_VS2022.vsix'
 Copy-Item $vsixSrcFilename -Destination $vsixDstFilename
 
-$appSrcFilename = $SolutionDir + 'SerialMonitor.Win.App\bin\Publish\net6.0-windows\publish\framework_dependent\SerialMonitor2.exe'
-$appDstFilename = $publishDir + 'SerialMonitor2_v' + $version + '_without_framework.exe'
-Copy-Item $appSrcFilename -Destination $appDstFilename
-
-$appSrcFilename = $SolutionDir + 'SerialMonitor.Win.App\bin\Publish\net6.0-windows\publish\self_contained\SerialMonitor2.exe'
+$appSrcFilename = $SolutionDir + 'SerialMonitor.Win.App\bin\Publish\net8.0-windows\publish\framework_dependent\SerialMonitor2.exe'
 $appDstFilename = $publishDir + 'SerialMonitor2_v' + $version + '.exe'
 Copy-Item $appSrcFilename -Destination $appDstFilename
 
