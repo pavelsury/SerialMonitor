abf6919b7bf0c97c811fc28b104a8f41b6be9074
Pavel Surý <suryp@email.cz>
2023-11-29 10:42:30 +0100

    Add Coffee tab

---
+++ SerialMonitor.Win.Ui/CoffeeControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace SerialMonitor.Win.Ui
+{
+    public partial class CoffeeControl : UserControl
+    {
+        public CoffeeControl()
+        {
+            InitializeComponent();
+        }
+
+        public string BuyMeACoffeeLink { get; } = "https://www.buymeacoffee.com/serialmonitor";
+
+        private void OnBuyMeACoffeeButtonClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(BuyMeACoffeeLink) { UseShellExecute = true });
+        }
+    }
+}
